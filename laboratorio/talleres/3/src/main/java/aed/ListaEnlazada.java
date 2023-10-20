d10cb9c5f6cc875824fa7f974ce5c05ccfa1adc7
arielbakal <arielbakal.dev@gmail.com>
2023-10-20 18:44:02 -0300

    L3 : hasta modificarPosicion()

--- laboratorio/talleres/3/src/main/java/aed/ListaEnlazada.java
+++ laboratorio/talleres/3/src/main/java/aed/ListaEnlazada.java
@@ -3,38 +3,110 @@ package aed;
 import java.util.*;
 
 public class ListaEnlazada<T> implements Secuencia<T> {
-    // Completar atributos privados
+    private Nodo actual;
+    private int size;
 
     private class Nodo {
-        // Completar
+        T valor;
+        Nodo sig;
+        Nodo ant;
+
+        Nodo(T v) {
+            valor = v;
+        };
     }
 
     public ListaEnlazada() {
-        throw new UnsupportedOperationException("No implementada aun");
+        actual = null;
     }
 
     public int longitud() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return size;
     }
 
     public void agregarAdelante(T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nuevo = new Nodo(elem);
+        // Conecto Nodo nuevo con Nodo actual
+        // Nuevo -> Actual
+        nuevo.sig = actual; 
+        // Mi Head ahora es Nodo nuevo
+        actual = nuevo;
+        size++;
     }
 
     public void agregarAtras(T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nuevo = new Nodo(elem);
+        // Si no tengo elementos => es mi primer nodo
+        if (actual == null) {
+            actual = nuevo;
+        } else {
+            // Nodo temporal para iterarlo
+            Nodo temp = actual;
+            // Itero hasta el ultimo nodo
+            while (temp.sig != null) {
+                temp = temp.sig;
+            }
+            // Conecto Nodo temp con Nodo nuevo
+            // temp -> nuevo
+            temp.sig = nuevo;
+            // Conecto Nodo nuevo con Nodo temp
+            // temp <- nuevo
+            nuevo.ant = temp;
+        }
+        size++;
     }
 
     public T obtener(int i) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo temp = actual;
+        // Itero hasta el i-esimo nodo
+        for (int j = 0; j < i; j++) {
+            temp = temp.sig;
+        }
+        return temp.valor;
     }
 
     public void eliminar(int i) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo temp = actual;
+        // Itero hasta el i-esimo nodo
+        for (int j = 0; j < i; j++) {
+            temp = temp.sig;
+        }
+        // Si tengo anterior => asigno al siguiente del anterior de temp el siguiente de temp.
+        // Basicamente conecto el Nodo anterior (temp.ant) con el Nodo siguiente (temp.sig), 
+        // descartando el Nodo intermedio (temp). 
+        // temp.ant <-> temp.sig
+        if (temp.ant != null) {
+            temp.ant.sig = temp.sig;
+        } else { 
+            // Si no tengo anterior => quiero eliminar el primero
+            actual = temp.sig;
+        }
+        // Si no tengo siguiente => quiero eliminar el ultimo
+        if (temp.sig != null) {
+            temp.sig.ant = temp.ant;
+        }
+        size--;
     }
 
     public void modificarPosicion(int indice, T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nuevo = new Nodo(elem);
+        Nodo temp = actual;
+        // Itero hasta el i-esimo nodo
+        for (int j = 0; j < indice; j++) {
+            temp = temp.sig;
+        }
+        // Nodo nuevo apunta al siguiente Nodo de temp
+        nuevo.sig = temp.sig;
+        // Si tengo anterior => conecto Nodo anterior al Nuevo
+        if (temp.ant != null) {
+            temp.ant.sig = nuevo;
+        } else {
+            // Si no tengo anterior => Asigno al primero
+            actual = nuevo;
+        }
+        temp.sig = nuevo;
+        nuevo.ant = temp;
+        // temp.ant <-> nuevo <-> temp.sig 
     }
 
     public ListaEnlazada<T> copiar() {
