83b7f756e6016faa70815b7f3807398abbe890b9
Ariel Bakal <arielbakal.dev@gmail.com>
2023-11-17 17:02:23 -0300

    T5: archivos subidos+}

---
+++ laboratorio/talleres/5/src/main/java/aed/InternetToolkit.java
@@ -0,0 +1,25 @@
+package aed;
+
+public class InternetToolkit {
+    public InternetToolkit() {
+    }
+
+    public Fragment[] tcpReorder(Fragment[] fragments) { // O(n)
+        // tiene que ordenar una lista de ids
+
+
+
+        return null;
+    }
+
+    public Router[] kTopRouters(Router[] routers, int k, int umbral) { // O( n + k*log n )
+        // IMPLEMENTAR
+        return null;
+    }
+
+    public IPv4Address[] sortIPv4(String[] ipv4) { // puede ser O(n^2), Posible radix sort
+        // IMPLEMENTAR
+        return null;
+    }
+
+}
